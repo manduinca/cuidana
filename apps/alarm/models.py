354d8fa887a8f9668a4bb5ffcda884e6cbed79f4
Luis Solier <luis.sajami@gmail.com>
2019-06-01 14:28:16 -0500

    migrations created

--- apps/alarm/models.py
+++ apps/alarm/models.py
@@ -8,34 +8,15 @@ class Patient(models.Model):
     medicament = models.CharField(max_length=50)
     created_date = models.DateTimeField(default=timezone.now)
 
-    def publish(self):
-        self.save()
 
-    def __str__(self):
-        return self.name
-
-
-class Alamr(models.Model):
-    patient = models.ForeignKey('models.Patient', on_delete=models.CASCADE)
+class Alarm(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     emision_date = models.DateTimeField(blank=True, null=True)
-    is_active = models.BooleanField(initial=True)
+    is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(default=timezone.now)
-
-    def publish(self):
-        self.save()
-
-    def __str__(self):
-        return self.emision_date
 
 
 class CareNetwork(models.Model):
     careman = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    patient = models.ForeignKey('models.Patient', on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
-
-    def publish(self):
-        self.save()
-
-    def __str__(self):
-        return self.careman
-
